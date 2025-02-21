2bcbe047c3f3b3ec53835fe1e266da5a424409fe
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-02-21 14:47:40 -0600

    feature gate kzg-intrinsics

--- bin/client-eth/src/main.rs
+++ bin/client-eth/src/main.rs
@@ -1,6 +1,7 @@
 use core::mem::transmute;
 
 use openvm::io::{println, read, reveal};
+#[allow(unused_imports)]
 use openvm_client_executor::{
     custom::{USED_BN_ADD, USED_BN_MUL, USED_BN_PAIR, USED_KZG_PROOF},
     io::ClientExecutorInput,
@@ -16,6 +17,7 @@ use {
     openvm_pairing_guest::{bls12_381::Bls12_381G1Affine, bn254::Bn254G1Affine},
 };
 
+#[cfg(feature = "kzg-intrinsics")]
 openvm_algebra_moduli_macros::moduli_init! {
     "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", // Bn254Fp Coordinate field
     "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", // Bn254 Scalar field
@@ -24,14 +26,33 @@ openvm_algebra_moduli_macros::moduli_init! {
     "0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab", // BLS12-381 Coordinate field
     "0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001" // BLS12-381 Scalar field
 }
+#[cfg(feature = "kzg-intrinsics")]
 openvm_ecc_sw_macros::sw_init! {
     Bn254G1Affine,
     Secp256k1Point,
     Bls12_381G1Affine,
 }
+#[cfg(feature = "kzg-intrinsics")]
 openvm_algebra_complex_macros::complex_init! {
     Bn254Fp2 { mod_idx = 0 },
     Bls12_381Fp2 { mod_idx = 4 },
+}
+
+#[cfg(not(feature = "kzg-intrinsics"))]
+openvm_algebra_moduli_macros::moduli_init! {
+    "0x30644e72e131a029b85045b68181585d97816a916871ca8d3c208c16d87cfd47", // Bn254Fp Coordinate field
+    "0x30644e72e131a029b85045b68181585d2833e84879b9709143e1f593f0000001", // Bn254 Scalar field
+    "0xFFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE FFFFFC2F", // secp256k1 Coordinate field
+    "0xFFFFFFFF FFFFFFFF FFFFFFFF FFFFFFFE BAAEDCE6 AF48A03B BFD25E8C D0364141", // secp256k1 Scalar field
+}
+#[cfg(not(feature = "kzg-intrinsics"))]
+openvm_ecc_sw_macros::sw_init! {
+    Bn254G1Affine,
+    Secp256k1Point,
+}
+#[cfg(not(feature = "kzg-intrinsics"))]
+openvm_algebra_complex_macros::complex_init! {
+    Bn254Fp2 { mod_idx = 0 },
 }
 
 pub fn main() {
@@ -70,6 +91,7 @@ pub fn main() {
     if unsafe { USED_BN_PAIR } {
         setup_complex_0(); // Bn254 complex extension of coordinate field
     }
+    #[cfg(feature = "kzg-intrinsics")]
     if unsafe { USED_KZG_PROOF } {
         setup_4(); // Bls12-381 coordinate field
         setup_5(); // Bls12-381 scalar field
