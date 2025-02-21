363d4929ea305f9e6d506d2bacd7552b3131c1b7
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-02-21 14:55:30 -0600

    chore: update host

--- bin/host/src/main.rs
+++ bin/host/src/main.rs
@@ -75,6 +75,9 @@ struct HostArgs {
     /// Max cells per chip in segment for continuations
     #[arg(short, long, alias = "max_cells_per_chip_in_segment")]
     pub max_cells_per_chip_in_segment: Option<usize>,
+
+    #[arg(long)]
+    pub kzg_intrinsics: bool,
 }
 
 const OPENVM_CLIENT_ETH_ELF: &[u8] = include_bytes!("../elf/openvm-client-eth");
@@ -83,6 +86,7 @@ fn reth_vm_config(
     app_log_blowup: usize,
     max_segment_length: usize,
     max_cells_per_chip_in_segment: usize,
+    use_kzg_intrinsics: bool,
 ) -> SdkVmConfig {
     let mut system_config = SystemConfig::default()
         .with_continuations()
@@ -97,6 +101,22 @@ fn reth_vm_config(
     let bls_config = PairingCurve::Bls12_381.curve_config();
     // The builder will do this automatically, but we set it just in case.
     let rv32m = Rv32M { range_tuple_checker_sizes: int256.range_tuple_checker_sizes };
+    let mut supported_moduli = vec![
+        bn_config.modulus.clone(),
+        bn_config.scalar.clone(),
+        SECP256K1_CONFIG.modulus.clone(),
+        SECP256K1_CONFIG.scalar.clone(),
+    ];
+    let mut supported_complex_moduli = vec![bn_config.modulus.clone()];
+    let mut supported_curves = vec![bn_config.clone(), SECP256K1_CONFIG.clone()];
+    let mut supported_pairing_curves = vec![PairingCurve::Bn254];
+    if use_kzg_intrinsics {
+        supported_moduli.push(bls_config.modulus.clone());
+        supported_moduli.push(bls_config.scalar.clone());
+        supported_complex_moduli.push(bls_config.modulus.clone());
+        supported_curves.push(bls_config.clone());
+        supported_pairing_curves.push(PairingCurve::Bls12_381);
+    }
     SdkVmConfig::builder()
         .system(system_config.into())
         .rv32i(Default::default())
@@ -105,21 +125,10 @@ fn reth_vm_config(
         .keccak(Default::default())
         .sha256(Default::default())
         .bigint(int256)
-        .modular(ModularExtension::new(vec![
-            bn_config.modulus.clone(),
-            bn_config.scalar.clone(),
-            SECP256K1_CONFIG.modulus.clone(),
-            SECP256K1_CONFIG.scalar.clone(),
-            bls_config.modulus.clone(),
-            bls_config.scalar.clone(),
-        ]))
-        .fp2(Fp2Extension::new(vec![bn_config.modulus.clone(), bls_config.modulus.clone()]))
-        .ecc(WeierstrassExtension::new(vec![
-            bn_config.clone(),
-            SECP256K1_CONFIG.clone(),
-            bls_config.clone(),
-        ]))
-        .pairing(PairingExtension::new(vec![PairingCurve::Bn254, PairingCurve::Bls12_381]))
+        .modular(ModularExtension::new(supported_moduli))
+        .fp2(Fp2Extension::new(supported_complex_moduli))
+        .ecc(WeierstrassExtension::new(supported_curves))
+        .pairing(PairingExtension::new(supported_pairing_curves))
         .build()
 }
 
@@ -198,8 +207,12 @@ async fn main() -> eyre::Result<()> {
     let max_cells_per_chip_in_segment =
         args.max_cells_per_chip_in_segment.unwrap_or(((1 << 23) - 100) * 120);
 
-    let vm_config =
-        reth_vm_config(app_log_blowup, max_segment_length, max_cells_per_chip_in_segment);
+    let vm_config = reth_vm_config(
+        app_log_blowup,
+        max_segment_length,
+        max_cells_per_chip_in_segment,
+        args.kzg_intrinsics,
+    );
     let sdk = Sdk;
     let elf = Elf::decode(OPENVM_CLIENT_ETH_ELF, MEM_SIZE as u32)?;
     let exe = VmExe::from_elf(elf, vm_config.transpiler()).unwrap();
