1a338ef6c92ac61c69828e7f067d6e2cdb2efa52
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-04-29 18:20:16 -0700

    feat: client-executor with stateless validation

--- crates/executor/client/src/lib.rs
+++ crates/executor/client/src/lib.rs
@@ -1,108 +1,35 @@
-/// Client program input data types.
-pub mod io;
-#[macro_use]
-mod utils;
-
-pub mod custom;
-
-use std::{borrow::BorrowMut, fmt::Display};
-
-use custom::CustomEvmConfig;
-use eyre::eyre;
-use io::ClientExecutorInput;
-#[allow(unused_imports)]
-pub use openvm_mpt;
-use openvm_mpt::state::HashedPostState;
-use openvm_primitives::chain_spec;
-use reth_chainspec::ChainSpec;
-use reth_errors::ProviderError;
-use reth_ethereum_consensus::validate_block_post_execution as validate_block_post_execution_ethereum;
-use reth_evm::execute::{BlockExecutionOutput, BlockExecutorProvider, Executor};
-use reth_evm_ethereum::execute::EthExecutorProvider;
-use reth_evm_optimism::OpExecutorProvider;
-use reth_execution_types::ExecutionOutcome;
-use reth_optimism_consensus::validate_block_post_execution as validate_block_post_execution_optimism;
-#[allow(unused_imports)]
-pub use reth_primitives;
-use reth_primitives::{proofs, Block, BlockWithSenders, Bloom, Header, Receipt, Receipts, Request};
-use reth_stateless::validation::stateless_validation;
-use revm::{db::CacheDB, Database};
-use revm_primitives::{address, B256, U256};
+use alloy_consensus::Block;
+use reth_chainspec::MAINNET;
+use reth_primitives::TransactionSigned;
+use reth_primitives_traits::RecoveredBlock;
+use reth_stateless::{validation::stateless_validation, ExecutionWitness};
+use revm_primitives::B256;
+use serde::{Deserialize, Serialize};
 
 /// Chain ID for Ethereum Mainnet.
 pub const CHAIN_ID_ETH_MAINNET: u64 = 0x1;
+
+/// The input for the client to execute a block and fully verify the STF (state transition
+/// function).
+///
+/// Instead of passing in the entire state, we only pass in the state roots along with merkle proofs
+/// for the storage slots that were modified and accessed.
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
+pub struct ClientExecutorInput {
+    /// The current block (which will be executed inside the client).
+    pub current_block: RecoveredBlock<Block<TransactionSigned>>,
+    pub witness: ExecutionWitness,
+}
 
 /// An executor that executes a block inside a zkVM.
 #[derive(Debug, Clone, Default)]
 pub struct ClientExecutor;
 
-/// Implementation for Ethereum-specific execution/validation logic.
-#[derive(Debug)]
-pub struct EthereumVariant;
-
-/// EVM chain variants that implement different execution/validation rules.
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum ChainVariant {
-    /// Ethereum networks.
-    Ethereum,
-}
-
-impl ChainVariant {
-    /// Returns the chain ID for the given variant.
-    pub fn chain_id(&self) -> u64 {
-        match self {
-            ChainVariant::Ethereum => CHAIN_ID_ETH_MAINNET,
-        }
-    }
-
-    pub fn chain_spec(&self) -> ChainSpec {
-        match self {
-            ChainVariant::Ethereum => chain_spec::mainnet(),
-        }
-    }
-}
-
 impl ClientExecutor {
-    pub fn execute(&self, mut input: ClientExecutorInput) -> eyre::Result<B256>
-    where
-        V: Variant,
-    {
-        let chain_variant = ChainVariant::Ethereum;
-        let chain_spec = Arc::new(chain_variant.chain_spec());
-
+    pub fn execute(&self, input: ClientExecutorInput) -> eyre::Result<B256> {
+        let chain_spec = MAINNET.clone();
         let ClientExecutorInput { current_block, witness } = input;
         let block_hash = stateless_validation(current_block, witness, chain_spec)?;
         Ok(block_hash)
-    }
-}
-
-impl Variant for EthereumVariant {
-    fn spec() -> ChainSpec {
-        openvm_primitives::chain_spec::mainnet()
-    }
-
-    fn execute<DB>(
-        executor_block_input: &BlockWithSenders,
-        executor_difficulty: U256,
-        cache_db: DB,
-    ) -> eyre::Result<BlockExecutionOutput<Receipt>>
-    where
-        DB: Database<Error: Into<ProviderError> + Display>,
-    {
-        Ok(EthExecutorProvider::new(
-            Self::spec().into(),
-            CustomEvmConfig::from_variant(ChainVariant::Ethereum),
-        )
-        .executor(cache_db)
-        .execute((executor_block_input, executor_difficulty).into())?)
-    }
-
-    fn validate_block_post_execution(
-        block: &BlockWithSenders,
-        chain_spec: &ChainSpec,
-        receipts: &[Receipt],
-        requests: &[Request],
-    ) -> eyre::Result<()> {
-        Ok(validate_block_post_execution_ethereum(block, chain_spec, receipts, requests)?)
     }
 }
