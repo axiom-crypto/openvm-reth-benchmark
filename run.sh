8caebeeee85075f752cb0c3f16b6f744e4c16b80
Ayush Shukla <ayush@axiom.xyz>
2025-07-08 16:55:37 +0200

    fix: use debug directory for dev build

--- run.sh
+++ run.sh
@@ -34,4 +34,12 @@ esac
 export JEMALLOC_SYS_WITH_MALLOC_CONF="retain:true,background_thread:true,metadata_thp:always,dirty_decay_ms:-1,muzzy_decay_ms:-1,abort_conf:true"
 RUSTFLAGS=$RUSTFLAGS cargo build --bin openvm-reth-benchmark-bin --profile=$PROFILE --no-default-features --features=$FEATURES
 PARAMS_DIR="params"
-RUST_LOG="info,p3_=warn" OUTPUT_PATH="metrics.json" ./target/$PROFILE/openvm-reth-benchmark-bin --kzg-params-dir $PARAMS_DIR --mode $MODE --block-number $BLOCK_NUMBER --rpc-url $RPC_1 --cache-dir rpc-cache
+
+# Use target/debug if profile is dev
+if [ "$PROFILE" = "dev" ]; then
+    TARGET_DIR="debug"
+else
+    TARGET_DIR="$PROFILE"
+fi
+
+RUST_LOG="info,p3_=warn" OUTPUT_PATH="metrics.json" ./target/$TARGET_DIR/openvm-reth-benchmark-bin --kzg-params-dir $PARAMS_DIR --mode $MODE --block-number $BLOCK_NUMBER --rpc-url $RPC_1 --cache-dir rpc-cache
