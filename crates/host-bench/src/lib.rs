3a8c85b5684004f229355cf7ce50313ffad33f7c
Ubuntu <ubuntu@ip-172-31-18-208.ec2.internal>
2025-10-27 23:48:03 +0000

    fix: hacky fix to make the metered execution work

--- crates/host-bench/src/lib.rs
+++ crates/host-bench/src/lib.rs
@@ -321,6 +321,8 @@ pub async fn run_reth_benchmark(args: HostArgs, openvm_client_eth_elf: &[u8]) ->
                             info_span!("interpreter.execute_metered", group = program_name)
                                 .in_scope(|| interpreter.execute_metered(stdin, metered_ctx))?;
                         println!("Number of segments: {}", segments.len());
+
+                        std::mem::forget(segments);
                     }
                     BenchMode::ProveApp => {
                         let mut prover = sdk.app_prover(elf)?.with_program_name(program_name);
