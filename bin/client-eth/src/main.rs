4f5a68fdaf4e6b52b131252b310eebf8001acf75
Jonathan Wang <31040440+jonathanpwang@users.noreply.github.com>
2025-11-04 08:17:22 -0800

    clean: imports

--- bin/client-eth/src/main.rs
+++ bin/client-eth/src/main.rs
@@ -1,14 +1,5 @@
 use openvm::io::{println, read, reveal_bytes32};
 use openvm_client_executor::{io::ClientExecutorInput, ClientExecutor};
-// Imports needed by the linker, but clippy can't tell:
-#[allow(unused_imports, clippy::single_component_path_imports)]
-use {
-    k256::Secp256k1Point,
-    openvm_algebra_guest::IntMod,
-    openvm_keccak256_guest, // trigger extern native-keccak256
-    openvm_pairing::{bls12_381::Bls12_381G1Affine, bn254::Bn254G1Affine},
-    p256::P256Point,
-};
 
 openvm::init!();
 
